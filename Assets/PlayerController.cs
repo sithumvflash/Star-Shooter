e6fb31b781d0c250e9bb3e68779d7cf66a13e91b
sithum vihanga <wwevihanga@gmail.com>
2023-06-07 19:56:29 +0530

    Update PlayerController.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using unirtEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
-  // [SerializeField] InputAction movement;
+
   void Start()
   {
 
   }
 
-  // Update is called once per frame
+
   void Update()
   {
     float horizontalThrow = Input.GetAxis("Horizontal");
     // creating a float variable to get the input of GetAxis
     Debug.Log(horizontalThrow);
 
-    float verticalLift = Input.GetAxis("Vertical");
-    Debug.Log(verticalLift);
+    float verticalThrow = Input.GetAxis("Vertical");
+    Debug.Log(verticalThrow);
   }
 }
